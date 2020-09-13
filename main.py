133282e158dad14bfd8e81b79232c7a57d853d6e
loboda4450 <loboda4450@gmail.com>
2020-09-13 22:09:52 +0200

    matched json pattern

--- main.py
+++ main.py
@@ -27,20 +27,20 @@ async def platform_info(request):
     print('Reloaded platform info', datetime.now())
 
     return web.json_response({
-                            'System': uname.system,
-                            'Node': uname.node,
-                            'Release': uname.release,
-                            'Version': uname.version,
-                            'Machine': uname.machine,
-                            'Processor': uname.processor,
-                            'Boot time': str(datetime.fromtimestamp((psutil.boot_time()))),
-                            'Physical cores': psutil.cpu_count(logical=False),
-                            'Total cores': psutil.cpu_count(logical=True),
-                            'Max Frequency': f'{cpufreq.max:.2f} Mhz',
-                            'Min Frequency': f'{cpufreq.min:.2f} Mhz',
-                            'Total': await get_size(svmem.total),
-                            'Total swap': await get_size(swap.total),
-                        }, status=200)
+        'System': uname.system,
+        'Node': uname.node,
+        'Release': uname.release,
+        'Version': uname.version,
+        'Machine': uname.machine,
+        'Processor': uname.processor,
+        'Boot time': str(datetime.fromtimestamp((psutil.boot_time()))),
+        'Physical cores': psutil.cpu_count(logical=False),
+        'Total cores': psutil.cpu_count(logical=True),
+        'Max Frequency': f'{cpufreq.max:.2f} Mhz',
+        'Min Frequency': f'{cpufreq.min:.2f} Mhz',
+        'Total': await get_size(svmem.total),
+        'Total swap': await get_size(swap.total),
+    }, status=200)
 
 
 @routes.get('/smtool/api/v0.1/disks_info')
@@ -66,16 +66,24 @@ async def collect_info(request):
 async def network_info(request):
     print('Reloaded network info', datetime.now())
     interfaces = psutil.net_if_addrs()
-    return web.json_response({'interfaces': [{'v4':
-                                                  {'name': network,
-                                                   'address': interfaces[network][0].address,
-                                                   'broadcast': interfaces[network][0].broadcast,
-                                                   'mask': interfaces[network][0].netmask},
-                                              'v6': {'name': network,
-                                                     'address': interfaces[network][1].address,
-                                                     'broadcast': interfaces[network][1].broadcast,
-                                                     'mask': interfaces[network][1].netmask}
-                                              } for network in interfaces]}, status=200)
+    if platform.uname().system == 'Windows':
+        return web.json_response({'interfaces': [{'Name': network,
+                                                  'IPv4': interfaces[network][1].address,
+                                                  'Broadcast for IPv4': interfaces[network][1].broadcast,
+                                                  'Mask for v4': interfaces[network][1].netmask,
+                                                  'Address for IPv6': interfaces[network][0].address,
+                                                  'Multicast': interfaces[network][0].broadcast,
+                                                  'Mask for IPv6': interfaces[network][0].netmask
+                                                  } for network in interfaces]}, status=200)
+    else:
+        return web.json_response({'interfaces': [{'Name': network,
+                                                  'IPv4': interfaces[network][0].address,
+                                                  'Broadcast for IPv4': interfaces[network][0].broadcast,
+                                                  'Mask for v4': interfaces[network][0].netmask,
+                                                  'Address for IPv6': interfaces[network][1].address,
+                                                  'Multicast': interfaces[network][1].broadcast,
+                                                  'Mask for IPv6': interfaces[network][1].netmask
+                                                  } for network in interfaces]}, status=200)
 
 
 app = web.Application()
